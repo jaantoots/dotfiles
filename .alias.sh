36df8dc2409b4aca1bb906b4d09d7b64eb526c01
Jaan Toots <jaantoots@gmail.com>
2017-10-11 15:48:30 +0100

    Add bash aliases

---
+++ .alias.sh
@@ -0,0 +1,16 @@
+# custom alias
+if [[ "$(uname)" = "Darwin" ]]; then
+    alias ls='ls -FG'
+    alias l='ls -laFG'
+else
+    alias lock='i3lock -fe -r 16 -s 4'
+fi
+
+alias e='emacsclient -t'
+alias ec='emacsclient -c'
+
+# Start VirtualBox VM in the background
+startvm(){
+    [[ "$#" -eq 1 ]] || return 1
+    VirtualBox --startvm "$1" &
+}
