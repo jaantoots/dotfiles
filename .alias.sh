03c53d99ce733c04d3b82b45c5cee7d9c1797291
Jaan Toots <jaantoots@gmail.com>
2017-10-31 15:43:27 +0000

    Use alias for haikunate

--- .alias.sh
+++ .alias.sh
@@ -1,11 +1,12 @@
 # custom alias
-alias lock='i3lock -fe -r 16 -s 4'
-alias emacs='TERM=${TERM/256color/16color} emacs'
-alias emacsclient='TERM=${TERM/256color/16color} emacsclient'
 alias e='emacsclient -t'
 alias ec='emacsclient -c'
-alias wgetu="wget -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/61.0.3163.100 Chrome/61.0.3163.100 Safari/537.36'"
+alias emacs='TERM=${TERM/256color/16color} emacs'
+alias emacsclient='TERM=${TERM/256color/16color} emacsclient'
+alias haikunate='python3 -c "import haikunator; print(haikunator.Haikunator().haikunate(token_length=0))"'
+alias lock='i3lock -fe -r 16 -s 4'
 alias mpvd='mpv --screen=1'
+alias wgetu="wget -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/61.0.3163.100 Chrome/61.0.3163.100 Safari/537.36'"
 
 # start VirtualBox VM in the background
 startvm(){
