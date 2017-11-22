58f756ba5a6d5211ba00ffb91a8f6bf7240f6889
Jaan Toots <jaantoots@gmail.com>
2017-11-22 18:01:31 +0000

    Fix emacs alias for urxvt

--- .alias.sh
+++ .alias.sh
@@ -1,8 +1,8 @@
 # custom alias
 alias e='emacsclient -t'
 alias ec='emacsclient -c'
-alias emacs='TERM=${TERM/256color/16color} emacs'
-alias emacsclient='TERM=${TERM/256color/16color} emacsclient'
+alias emacs='TERM=${TERM%-256color} emacs'
+alias emacsclient='TERM=${TERM%-256color} emacsclient'
 alias haikunate='python3 -c "import haikunator; print(haikunator.Haikunator().haikunate(token_length=0))"'
 alias lock='i3lock -fe -r 16 -s 4'
 alias mpvd='mpv --screen=0 --fs-screen=1'
