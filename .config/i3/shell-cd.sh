05b5cb3c72f231fd86299e190fc85fdea6a9db39
Jaan Toots <jaantoots@gmail.com>
2018-06-26 20:12:11 +0100

    Rewrite shell-cd.sh using process substitution

--- .config/i3/shell-cd.sh
+++ .config/i3/shell-cd.sh
@@ -2,16 +2,13 @@
 
 cd="$HOME"
 
-window="$(xdpyinfo | grep -m 1 -E '^focus' | cut -d " " -f 4 | tr -d ',')"
-if [ -n "$window" ]; then
-    wpid="$(xprop -id "$window" | grep -m 1 PID | cut -d " " -f 3)"
-fi
-
-if [ -n "$wpid" ]; then
-    while read pid; do
-        grep -q "$(ps -o comm= -p "$pid")" /etc/shells && break
-    done <<<"$(ps --ppid "$wpid" -o pid=)"
-fi
+while read pid; do
+    grep -q "$(ps -o comm= -p "$pid")" /etc/shells && break
+done < <(
+    xdpyinfo | grep -m 1 -E '^focus' | cut -d " " -f 4 | tr -d ',' | \
+        xargs -r xprop -id | grep -m 1 PID | cut -d " " -f 3 | \
+        xargs -r ps -o pid= --ppid
+)
 
 if [ -n "$pid" ]; then
     cd="$(readlink "/proc/$pid/cwd")"
