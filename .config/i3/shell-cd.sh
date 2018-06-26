27be02265614290a6419d985ed1c5631103e26ff
Jaan Toots <jaantoots@gmail.com>
2018-06-26 18:59:25 +0100

    Fix shell-cd.sh script for multiple child processes

--- .config/i3/shell-cd.sh
+++ .config/i3/shell-cd.sh
@@ -6,13 +6,15 @@ window="$(xdpyinfo | grep -m 1 -E '^focus' | cut -d " " -f 4 | tr -d ',')"
 if [ -n "$window" ]; then
     wpid="$(xprop -id "$window" | grep -m 1 PID | cut -d " " -f 3)"
 fi
+
 if [ -n "$wpid" ]; then
-    pid="$(ps --ppid "$wpid" -o pid=)"
+    while read pid; do
+        grep -q "$(ps -o comm= -p "$pid")" /etc/shells && break
+    done <<<"$(ps --ppid "$wpid" -o pid=)"
 fi
 
 if [ -n "$pid" ]; then
-    grep -q "$(readlink "/proc/$pid/exe")" /etc/shells \
-        && cd="$(readlink "/proc/$pid/cwd")"
+    cd="$(readlink "/proc/$pid/cwd")"
 fi
 
 echo "$cd"
