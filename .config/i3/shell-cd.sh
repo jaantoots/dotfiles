c46448f486ae944236edacfe5e5b49086c133dc8
Jaan Toots <jaantoots@gmail.com>
2018-06-26 21:02:18 +0100

    Improve shell-cd.sh performance by using only one ps call

--- .config/i3/shell-cd.sh
+++ .config/i3/shell-cd.sh
@@ -2,12 +2,12 @@
 
 cd="$HOME"
 
-while read pid; do
-    grep -q "$(ps -o comm= -p "$pid")" /etc/shells && break
+while read pid cmd; do
+    grep -q "$(cut -d " " -f 1 <<<"$cmd")" /etc/shells && break
 done < <(
     xdpyinfo | grep -m 1 -E '^focus' | cut -d " " -f 4 | tr -d ',' | \
         xargs -r xprop -id | grep -m 1 PID | cut -d " " -f 3 | \
-        xargs -r ps -o pid= --ppid
+        xargs -r ps -o pid=,cmd= --ppid
 )
 
 if [ -n "$pid" ]; then
