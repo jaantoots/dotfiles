6f14239d206bbe4e5cba1b8e15cdd3e515d82d5e
Jaan Toots <jaantoots@gmail.com>
2018-07-01 00:44:41 +0100

    Add vim alias and update formatting

--- .alias.sh
+++ .alias.sh
@@ -1,8 +1,9 @@
 # custom alias
-alias e='emacsclient -t'
-alias ec='emacsclient -c'
-alias haikunate='python3 -c "import haikunator; print(haikunator.Haikunator().haikunate(token_length=0))"'
-alias mpvd='mpv --screen=0 --fs-screen=1'
+alias v="vim"
+alias e="emacsclient -t"
+alias ec="emacsclient -c"
+alias haikunate="python3 -c 'import haikunator; print(haikunator.Haikunator().haikunate(token_length=0))'"
+alias mpvd="mpv --screen=0 --fs-screen=1"
 alias wgetu="wget -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/61.0.3163.100 Chrome/61.0.3163.100 Safari/537.36'"
 alias zathura="zathura --fork"
 
