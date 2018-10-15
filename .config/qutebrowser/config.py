4494c55ecd8916b97698ffc2e09fc4035cf331c8
Jaan Toots <jaan@jaantoots.org>
2018-10-15 19:45:25 +0100

    Use manual config.yml for qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,8 +1,14 @@
 # pylint: disable=C0111
+import yaml
 c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
 
-config.load_autoconfig()
+# Load config.yml
+with (config.configdir / 'config.yml').open() as f:
+    yaml_data = yaml.load(f)
+
+for k, v in yaml_data.items():
+    config.set(k, v)
 
 # Load color theme
-config.source("base16-qutebrowser/themes/base16-default-dark.config.py")
+config.source('base16-qutebrowser/themes/base16-default-dark.config.py')
