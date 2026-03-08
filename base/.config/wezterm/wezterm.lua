8cdadc707c65ea9f7ebe368d54492bcc64594c89
Jaan Toots <jaan.toots@pactum.com>
2026-03-08 23:25:44 +0200

    Update WezTerm color scheme, add Github link rule

--- base/.config/wezterm/wezterm.lua
+++ base/.config/wezterm/wezterm.lua
@@ -1,19 +1,31 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE"
-config.color_scheme = 'Default Dark (base16)'
+config.color_scheme = "Tokyo Night"
 
 config.keys = {
-  {
-    key = 't',
-    mods = 'SHIFT|SUPER',
-    action = wezterm.action.SpawnCommandInNewTab {
-      domain = 'DefaultDomain',
-      cwd = wezterm.home_dir,
-    },
-  },
-  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
+	{
+		key = "t",
+		mods = "SHIFT|SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			domain = "DefaultDomain",
+			cwd = wezterm.home_dir,
+		}),
+	},
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
 }
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wezterm/wezterm | "wezterm/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+	regex = [[["]?(pactum-ai)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://www.github.com/$1/$3",
+})
 
 return config
